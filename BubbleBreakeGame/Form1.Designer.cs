6c09dacd37502ea5a3be884e21060cfb085fb875
Pubudusandeepa <39061732+Pubudusandeepa@users.noreply.github.com>
2019-11-11 23:14:29 +0530

    first commit

---
+++ BubbleBreakeGame/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace BubbleBreakeGame
+{
+    partial class frmBubbleBreaker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblInf = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(219, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 0;
+            // 
+            // lblInf
+            // 
+            this.lblInf.AutoSize = true;
+            this.lblInf.Location = new System.Drawing.Point(184, 155);
+            this.lblInf.Name = "lblInf";
+            this.lblInf.Size = new System.Drawing.Size(0, 13);
+            this.lblInf.TabIndex = 1;
+            // 
+            // frmBubbleBreaker
+            // 
+            this.ClientSize = new System.Drawing.Size(773, 409);
+            this.Controls.Add(this.lblInf);
+            this.Controls.Add(this.label2);
+            this.Name = "frmBubbleBreaker";
+            this.Load += new System.EventHandler(this.frmBubbleBreaker_load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form_Paint1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblInf;
+    }
+}
+
---
+++ BubbleBreakeGame/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleBreakeGame
+{
+    public partial class frmBubbleBreaker : Form
+    {
+        enum Colors
+        {
+            None,
+            Red,
+            Green,
+            Yellow,
+            Blue,
+            Purple
+        };
+
+        const int NUM_BUBBLES = 10;
+        const int BUBBLE_SIZE = 50;
+        Colors[,] colors;
+        Random rand;
+        int score;
+        bool[,] isSelected;
+        int numOfSelectedBubbles;
+
+        public frmBubbleBreaker()
+        {
+            InitializeComponent();
+            rand = new Random();
+            numOfSelectedBubbles = 0;
+            score = 0;
+            colors = new Colors[NUM_BUBBLES, NUM_BUBBLES];
+            isSelected = new bool[NUM_BUBBLES, NUM_BUBBLES];
+            lblInf.BackColor = Color.White;
+
+        }
+
+        private void frmBubbleBreaker_load(object sender, EventArgs e)
+        {
+            SetClientSizeCore(NUM_BUBBLES * BUBBLE_SIZE, NUM_BUBBLES * BUBBLE_SIZE);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            BackColor = Color.Black;
+            Start();
+        }
+
+
+
+
+        private void Start()
+        {
+            for(int row =0; row < NUM_BUBBLES; row++)
+            {
+                for(int col=0; col < NUM_BUBBLES; col++)
+                {
+                    colors[row, col] = (Colors)rand.Next(1, 6);
+                }
+            }
+
+            this.Text = score + "points";
+
+        }
+
+        private void Form_Paint1(object sender, PaintEventArgs e)
+        {
+            for (int row = 0; row < NUM_BUBBLES; row++)
+            {
+                for (int col = 0; col < NUM_BUBBLES; col++)
+                {
+                    Color bubbleColor = Color.Empty;
+
+                    switch (colors[row, col])
+                    {
+                        case Colors.Red:
+                            bubbleColor = Color.Red;
+                            break;
+                        case Colors.Yellow:
+                            bubbleColor = Color.Yellow;
+                            break;
+                        case Colors.Green:
+                            bubbleColor = Color.Green;
+                            break;
+                        case Colors.Purple:
+                            bubbleColor = Color.Purple;
+                            break;
+
+                    }
+                    e.Graphics.FillEllipse(new LinearGradientBrush(
+                        new Point(row * BUBBLE_SIZE, col * BUBBLE_SIZE),
+                        new Point(row * BUBBLE_SIZE + BUBBLE_SIZE, col * BUBBLE_SIZE + BUBBLE_SIZE),
+                        Color.White, bubbleColor), row * BUBBLE_SIZE, col * BUBBLE_SIZE, BUBBLE_SIZE, BUBBLE_SIZE);
+
+                }
+            }
+        }
+    }
+}
